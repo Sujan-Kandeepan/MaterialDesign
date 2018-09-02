676087dee1888f239d74f35afba46af87791b46d
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-09-02 02:33:22 -0400

    Added collapsing toolbar layout demo app

---
+++ CollapsingToolbarLayout/app/src/main/java/com/example/sujankandeepan/collapsingtoolbarlayout/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sujankandeepan.collapsingtoolbarlayout;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Added to favorites", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
